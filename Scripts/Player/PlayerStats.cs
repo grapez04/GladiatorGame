54d466f38ce1d199405a7511111b73982bc31b07
benj381a <118054799+benj381a@users.noreply.github.com>
2025-12-06 15:29:51 +0100

    Update PlayerStats.cs

--- Scripts/Player/PlayerStats.cs
+++ Scripts/Player/PlayerStats.cs
@@ -4,4 +4,13 @@ public class PlayerStats : Entity
 {
     public float defaultMoveSpeed = 5f;
     public float defaultAttackRange = 1f;
+
+    public PlayerStats(float _health = 1f, float _attackDamage = 1f, float _speed = 1f, float _defaultMoveSpeed = 5f, float _defaultAttackRange = 1f)
+    {
+        health = _health;
+        attackDamage = _attackDamage;
+        speed = _speed;
+        defaultMoveSpeed = _defaultMoveSpeed;
+        defaultAttackRange = _defaultAttackRange;
+    }
 }
