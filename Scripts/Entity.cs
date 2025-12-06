959f5103cc5db7e67cabb5b077e7322935a60df9
benj381a <118054799+benj381a@users.noreply.github.com>
2025-12-06 10:40:09 +0100

    create Entity.cs

---
+++ Scripts/Entity.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public float health = 1f;
+    public float attackDamage = 1f;
+    public float speed = 1f;
+
+    public void Attack()
+    {
+
+    }
+}
