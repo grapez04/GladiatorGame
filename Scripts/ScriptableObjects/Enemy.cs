5c3301299ac19e307f2ada35ed958f9f21479010
grapez04 <benbix123@gmail.com>
2025-12-07 00:55:41 +0100

    enemyScriptableObjects

--- Scripts/ScriptableObjects/Enemy.cs
+++ Scripts/ScriptableObjects/Enemy.cs
@@ -6,6 +6,8 @@ public class Enemy: ScriptableObject
 {
     public Sprite sprite;
     public RuntimeAnimatorController controller;
+    
+    [Space]
 
     public int attackDamage = 1;
     public float speed = 2;
